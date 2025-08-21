c16b4e98537e257be5e06a8281d58f4e61e0f812
ort <orthodoxplus@ya.ru>
2025-08-21 06:57:13 +0300

    git-dir-copy

--- src/main/java/art/aelaort/service/GitDirCopyService.java
+++ src/main/java/art/aelaort/service/GitDirCopyService.java
@@ -2,20 +2,27 @@ package art.aelaort.service;
 
 import art.aelaort.exceptions.AppExitErrorException;
 import art.aelaort.exceptions.AppPrintUsageException;
-import org.springframework.beans.factory.annotation.Value;
+import art.aelaort.utils.Utils;
+import art.aelaort.utils.system.Response;
+import art.aelaort.utils.system.SystemProcess;
+import lombok.RequiredArgsConstructor;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
 import org.springframework.stereotype.Component;
 
 import java.nio.file.Files;
 import java.nio.file.InvalidPathException;
 import java.nio.file.Path;
+import java.util.Optional;
 
 import static art.aelaort.utils.ColoredConsoleTextUtils.wrapRed;
 import static art.aelaort.utils.Utils.log;
 
 @Component
+@RequiredArgsConstructor
 public class GitDirCopyService {
-	@Value("${tmp.root.dir}")
-	private Path tmpDir;
+	private final SystemProcess systemProcess;
+	private final Utils utils;
 
 	public void copy(String[] args) {
 		if (args.length != 2) {
@@ -34,7 +41,35 @@ public class GitDirCopyService {
 	public void copy(Path gitDir, Path bundlePath) {
 		checkParams(gitDir, bundlePath);
 
-		
+		Path tmp = utils.createTmpDir();
+		Response response = extractBundle(bundlePath, tmp);
+		if (response.exitCode() != 0) {
+			log(wrapRed("git clone error"));
+			throw new AppExitErrorException();
+		}
+
+		Optional<Path> gitDirOp = getGitDir(tmp, bundlePath);
+
+
+		FileUtils.deleteQuietly(tmp.toFile());
+	}
+
+	private static Optional<Path> getGitDir(Path tmp, Path bundlePath) {
+		String projectName = FilenameUtils.removeExtension(bundlePath.getFileName().toString());
+		Path projectDir = tmp.resolve(projectName);
+		if (Files.notExists(projectDir)) {
+			return Optional.empty();
+		}
+
+		Path gitDir = projectDir.resolve(".git");
+		if (Files.notExists(gitDir)) {
+			return Optional.empty();
+		}
+		return Optional.of(gitDir);
+	}
+
+	private Response extractBundle(Path bundlePath, Path tmp) {
+		return systemProcess.callProcess(tmp, "git clone --no-checkout " + bundlePath.toString());
 	}
 
 	private static void checkParams(Path gitDir, Path bundlePath) {
--- src/main/java/art/aelaort/utils/Utils.java
+++ src/main/java/art/aelaort/utils/Utils.java
@@ -3,7 +3,11 @@ package art.aelaort.utils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Arrays;
+import java.util.UUID;
 
 @Component
 public class Utils {
@@ -15,6 +19,22 @@ public class Utils {
 	private String dockerImagePattern1Replacement;
 	@Value("${servers.management.docker-reg-name-2.replacement}")
 	private String dockerImagePattern2Replacement;
+	@Value("${tmp.root.dir}")
+	private Path tmpRootDir;
+
+	public Path createTmpDir() {
+		return createTmpDir(tmpRootDir);
+	}
+
+	public static Path createTmpDir(Path tmpRootDir) {
+		try {
+			Path path = tmpRootDir.resolve(UUID.randomUUID().toString());
+			Files.createDirectory(path);
+			return path;
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
 
 	public static void log(String format, Object... args) {
 		System.out.printf(format, args);
