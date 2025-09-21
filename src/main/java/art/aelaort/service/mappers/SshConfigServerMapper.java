f53a0eed14ebd54afe6880e4a2f9609dffc3d6ce
ort <orthodoxplus@ya.ru>
2025-09-21 18:51:44 +0300

    ssh config instead tabby

--- src/main/java/art/aelaort/service/mappers/SshConfigServerMapper.java
+++ src/main/java/art/aelaort/service/mappers/SshConfigServerMapper.java
@@ -2,27 +2,30 @@ package art.aelaort.service.mappers;
 
 import art.aelaort.models.servers.ssh.SshConfigServer;
 import org.apache.sshd.client.config.hosts.HostConfigEntry;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import java.nio.file.Path;
 import java.util.List;
+import java.util.Map;
 
 @Component
 public class SshConfigServerMapper {
-	public List<SshConfigServer> map(List<HostConfigEntry> configEntries) {
+	@Value("${ssh.common.config.key_file_prefix}")
+	private String sshCommonConfigKeyFilePrefix;
+
+	public List<SshConfigServer> map(List<HostConfigEntry> configEntries, Map<String, String> serverNamesByHost) {
 		return configEntries.stream()
 				.map(e -> {
 					String key = "";
-					String name = e.getHost();
 					if (!e.getIdentities().isEmpty()) {
-						String keyFile = e.getIdentities().iterator().next();
-						key = keyFile;
-						name = Path.of(keyFile).getFileName().toString().split("\\.")[0];
+						key = e.getIdentities().iterator().next();
 					}
 					return new SshConfigServer(
-							name,
+							serverNamesByHost.getOrDefault(e.getHost(), e.getHost()),
 							e.getHostName(),
-							key,
+							key
+									.replace(sshCommonConfigKeyFilePrefix, "")
+									.replace(".pub", ""),
 							e.getPort()
 					);
 				})
--- src/main/java/art/aelaort/service/providers/SshConfigServerProvider.java
+++ src/main/java/art/aelaort/service/providers/SshConfigServerProvider.java
@@ -8,8 +8,12 @@ import org.apache.sshd.client.config.hosts.HostConfigEntry;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Component
 @RequiredArgsConstructor
@@ -17,14 +21,29 @@ public class SshConfigServerProvider {
 	private final SshConfigServerMapper sshConfigServerMapper;
 	@Value("${ssh.common.config}")
 	private Path sshCommonConfig;
+	@Value("${ssh.common.config-names}")
+	private Path sshCommonConfigNames;
+
 
 	public List<SshConfigServer> readLocal() {
 		List<HostConfigEntry> hostConfigEntries = parseFile(sshCommonConfig);
-		return sshConfigServerMapper.map(hostConfigEntries);
+		Map<String, String> serverNamesByHost = readHostNames(sshCommonConfigNames);
+		return sshConfigServerMapper.map(hostConfigEntries, serverNamesByHost);
 	}
 
 	@SneakyThrows
 	private List<HostConfigEntry> parseFile(Path configPath) {
 		return HostConfigEntry.readHostConfigEntries(configPath);
+	}
+
+	@SneakyThrows
+	private Map<String, String> readHostNames(Path configNamesPath) {
+		try (Stream<String> lines = Files.lines(configNamesPath)) {
+			return lines
+					.map(String::trim)
+					.filter(s -> !s.isEmpty())
+					.map(line -> line.split("="))
+					.collect(Collectors.toMap(arr -> arr[0], arr -> arr[1]));
+		}
 	}
 }
